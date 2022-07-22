be9f3e006719a12b71adbb03520c13e19c57c259
Emanuel Onofre <emanuelonofre@csus.edu>
2022-07-21 17:12:40 -0700

    Building city search component part 2

--- src/api.js
+++ src/api.js
@@ -1,12 +1,10 @@
 export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a470befc3cmsh036d8e5a4631c35p1ddb2djsnb44e6dd9acbf',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a470befc3cmsh036d8e5a4631c35p1ddb2djsnb44e6dd9acbf",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
 };
 
-fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
