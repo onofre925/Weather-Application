a08c9ae899d05529b86aa7abfb31b5ce6325317b
Emanuel Onofre <emanuelonofre@csus.edu>
2022-07-22 15:30:18 -0700

    Building current weather component pt.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
+
+
+
 function App() {
 
   const handleOnSearchChange = (searchData) =>{
@@ -8,6 +12,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
