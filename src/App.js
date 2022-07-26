02c9526ed9cbab09a3c918da728ab362f9d85cc7
Emanuel Onofre <emanuelonofre@csus.edu>
2022-07-25 18:45:51 -0700

    Fetching and mapping data from weather API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL, WEATHER_API_KEY} from './api';
 
 
 
 function App() {
 
   const handleOnSearchChange = (searchData) =>{
-    console.log(searchData);
+   const [lat, lon ] =  searchData.value.split(" ");
+
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+    Promise.all({currentWeatherFetch, forecastFetch})
+      .then(async (response) => {
+        const weatherReponse = await reponse[0].json();
+        const forecastReponse = await reponse[1].json();
+
+      })
+    }
+
   }
   return (
     <div className="container">
